2e555a9925e3d8f6467fcccb0fc4be4e3bfcf2a8
Leonardo Duarte <leocc_etraud@outlook.com>
2025-09-08 16:39:48 -0300

    fix: correção de rota de API no Axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,8 @@ import { env } from "@/env";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3333",
+  baseURL:
+    import.meta.env.VITE_API_URL ?? "https://pizzashop-back-end.onrender.com",
   withCredentials: true, // <- importante para CORS + cookies
   headers: { "Content-Type": "application/json" },
 });
